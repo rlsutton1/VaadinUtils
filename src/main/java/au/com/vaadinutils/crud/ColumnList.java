3d8c63de08964fd41783f948172eed882ab6128b
bsutton@noojee.com.au <bsutton@noojee.com.au>
2013-09-04 23:41:46 +1000

    builds a list of entity column attributes which can be passed around.

---
+++ src/main/java/au/com/vaadinutils/crud/ColumnList.java
@@ -0,0 +1,26 @@
+package au.com.vaadinutils.crud;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.metamodel.SingularAttribute;
+
+public class ColumnList
+{
+	List<SingularAttribute<? extends Object, ? extends Object>> columns = new ArrayList<SingularAttribute<? extends Object, ? extends Object>>();
+
+	@SafeVarargs
+	public ColumnList(SingularAttribute<? extends Object, ? extends Object>... columns)
+	{
+		for (SingularAttribute<? extends Object, ? extends Object> column : columns)
+		{
+			this.columns.add(column);
+		}
+	}
+
+	public List<SingularAttribute<? extends Object, ? extends Object>>  getList()
+	{
+		return columns;
+	}
+
+}
