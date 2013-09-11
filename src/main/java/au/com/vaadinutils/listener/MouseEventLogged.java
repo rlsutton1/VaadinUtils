033e0c235b46522c54f577f9c7f44173db93d2d0
bsutton@noojee.com.au <bsutton@noojee.com.au>
2013-09-11 23:10:02 +1000

    Added support for logged MouseEvent listener.

---
+++ src/main/java/au/com/vaadinutils/listener/MouseEventLogged.java
@@ -0,0 +1,59 @@
+package au.com.vaadinutils.listener;
+
+import org.apache.log4j.Logger;
+
+public class MouseEventLogged
+{
+	static public abstract class ClickListener implements com.vaadin.event.MouseEvents.ClickListener
+	{
+		private static final long serialVersionUID = 7420365324169589382L;
+
+		Logger logger = Logger.getLogger(ClickListener.class);
+
+		abstract public void clicked(com.vaadin.event.MouseEvents.ClickEvent event);
+
+		@Override
+		public void click(com.vaadin.event.MouseEvents.ClickEvent event)
+		{
+			try
+			{
+				clicked(event);
+			}
+			catch (Throwable e)
+			{
+				logger.error(e, e);
+				throw new RuntimeException(e);
+			}
+
+		}
+
+	}
+
+	static public class ClickAdaptor implements com.vaadin.event.MouseEvents.ClickListener
+	{
+		private static final long serialVersionUID = 1L;
+
+		Logger logger = Logger.getLogger(ClickAdaptor.class);
+
+		private com.vaadin.event.MouseEvents.ClickListener listener = null;
+
+		public ClickAdaptor(com.vaadin.event.MouseEvents.ClickListener listener)
+		{
+			this.listener = listener;
+		}
+
+		public void click(com.vaadin.event.MouseEvents.ClickEvent event)
+		{
+			try
+			{
+				listener.click(event);
+			}
+			catch (Throwable e)
+			{
+				logger.error(e, e);
+				throw new RuntimeException(e);
+			}
+
+		}
+	}
+}
