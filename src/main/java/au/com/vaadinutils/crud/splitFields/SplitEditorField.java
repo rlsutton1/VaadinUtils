7ae1b002698b9f5f47810898ebd4a41fbc0ba1dc
bsutton@noojee.com.au <bsutton@noojee.com.au>
2013-09-15 07:41:35 +1000

    Added support for ckeditor.

---
+++ src/main/java/au/com/vaadinutils/crud/splitFields/SplitEditorField.java
@@ -0,0 +1,37 @@
+package au.com.vaadinutils.crud.splitFields;
+
+import au.com.vaadinutils.fields.CKEditorEmailField;
+
+import com.vaadin.ui.Label;
+
+public class SplitEditorField extends CKEditorEmailField implements SplitField
+{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7753660388792217050L;
+
+	public SplitEditorField(boolean readonly)
+	{
+		super(readonly);
+	}
+
+	@Override
+	public void setVisible(boolean visible)
+	{
+		super.setVisible(visible);
+	}
+
+	@Override
+	public Label getLabel()
+	{
+		return null;
+	}
+
+	@Override
+	public String getCaption()
+	{
+		return null;
+	}
+
+}
