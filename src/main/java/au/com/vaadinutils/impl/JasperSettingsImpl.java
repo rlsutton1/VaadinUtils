fc7b9def7e84e32256066c2c0139ff9ada584a8b
bsutton@noojee.com.au <bsutton@noojee.com.au>
2013-09-18 09:13:11 +1000

    Added hooks that allow a dev to provide some common interfaces used throughout the library.

---
+++ src/main/java/au/com/vaadinutils/impl/JasperSettingsImpl.java
@@ -0,0 +1,85 @@
+package au.com.vaadinutils.impl;
+
+import java.io.File;
+
+import au.org.scoutmaster.jasper.JasperSettings;
+
+/**
+ * If you want to use the Jasper Wizard you need to implement this class.
+ * @author bsutton
+ *
+ */
+public class JasperSettingsImpl implements JasperSettings
+{
+
+	@Override
+	public String getReportDir()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public File getDocumentBase()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public File getInitParameterRealPath(String paramName)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getSmtpFQDN()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Integer getSmtpPort()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isAuthRequired()
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String getUsername()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getPassword()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean getUseSSL()
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String getBounceEmailAddress()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/main/java/au/com/vaadinutils/impl/LocalEntityManagerFactory.java
@@ -0,0 +1,45 @@
+package au.com.vaadinutils.impl;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+/**
+ * You need to hook this class as a servlet context listener in your web.xml
+ * 
+ * You then need to create an EntityManagerFactory which the rest of the code relies on.
+ * 
+ * @author bsutton
+ *
+ */
+@WebListener  
+public class LocalEntityManagerFactory implements ServletContextListener
+{
+	private static EntityManagerFactory emf;
+
+	@Override
+	public void contextInitialized(ServletContextEvent event)
+	{
+		// TODO replace this exception with something like:
+		// emf = Persistence.createEntityManagerFactory("scoutmaster");
+		throw new IllegalStateException("You need to implement this method!");
+	}
+
+	@Override
+	public void contextDestroyed(ServletContextEvent event)
+	{
+		emf.close();
+	}
+
+	public static EntityManager createEntityManager()
+	{
+		if (emf == null)
+		{
+			throw new IllegalStateException("Context is not initialized yet.");
+		}
+
+		return emf.createEntityManager();
+	}
+}
