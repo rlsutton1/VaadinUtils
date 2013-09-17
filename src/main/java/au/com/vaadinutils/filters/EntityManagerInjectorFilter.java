331282266e176507717b32ebae9866faff26cac2
bsutton@noojee.com.au <bsutton@noojee.com.au>
2013-09-18 09:58:15 +1000

    reorganised package names.

---
+++ src/main/java/au/com/vaadinutils/filters/EntityManagerInjectorFilter.java
@@ -0,0 +1,85 @@
+package au.com.vaadinutils.filters;
+
+import java.io.IOException;
+
+import javax.persistence.EntityManager;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+
+import org.apache.log4j.Logger;
+
+import au.com.vaadinutils.dao.EntityManagerProvider;
+import au.com.vaadinutils.dao.Transaction;
+import au.com.vaadinutils.impl.LocalEntityManagerFactory;
+
+public class EntityManagerInjectorFilter implements Filter
+{
+	private static Logger logger = Logger.getLogger(EntityManagerInjectorFilter.class);
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException
+	{
+	}
+
+	@Override
+	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+			throws IOException, ServletException
+	{
+		EntityManager em = LocalEntityManagerFactory.createEntityManager();
+		
+		Transaction t = new Transaction(em);
+		try 
+		{
+			// Create and set the entity manager
+			EntityManagerProvider.INSTANCE.setCurrentEntityManager(em);
+
+			// Handle the request
+			filterChain.doFilter(servletRequest, servletResponse);
+
+			t.commit();
+		}
+		catch (ConstraintViolationException  e)
+		{
+			if (e.getCause() instanceof ConstraintViolationException)
+			{
+				e = (ConstraintViolationException) e.getCause();
+			}
+
+			if (e != null)
+			{
+				for (ConstraintViolation<?> violation : e.getConstraintViolations())
+				{
+					StringBuilder sb = new StringBuilder();
+					sb.append("Constraint Violation: \n");
+					sb.append("Entity:" + violation.getRootBean());
+					sb.append("Error: " + violation.getMessage() + "\n");
+					sb.append(" on property: " + violation.getPropertyPath() + "\n");
+					sb.append("Constraint:" + violation.getMessageTemplate());
+					
+					logger.error(sb.toString());
+				}
+
+			}
+			throw e;
+		}
+		finally
+		{
+			if (t!= null)
+				t.close();
+			// Reset the entity manager
+			EntityManagerProvider.INSTANCE.setCurrentEntityManager(null);
+		}
+	}
+
+	@Override
+	public void destroy()
+	{
+		// entityManagerFactory = null;
+	}
+}
