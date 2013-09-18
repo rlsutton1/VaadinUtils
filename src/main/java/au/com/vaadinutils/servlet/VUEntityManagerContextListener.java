7d37a102b72c5ef4cd871049cf6a1dff27e38fe8
bsutton@noojee.com.au <bsutton@noojee.com.au>
2013-09-19 09:55:52 +1000

    Restructured handling of EntityManagerFactory.

---
+++ src/main/java/au/com/vaadinutils/servlet/VUEntityManagerContextListener.java
@@ -0,0 +1,48 @@
+
+package au.com.vaadinutils.servlet;
+
+import javax.persistence.EntityManagerFactory;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import au.com.vaadinutils.dao.EntityManagerProvider;
+
+/**
+ * You need to hook this class as a servlet context listener in your web.xml
+ * 
+ * You then need to create an EntityManagerFactory which the rest of the code
+ * relies on.
+ * 
+ * @author bsutton
+ * 
+ */
+public abstract class VUEntityManagerContextListener implements ServletContextListener
+{
+	private static EntityManagerFactory emf;
+
+	@Override
+	public void contextInitialized(ServletContextEvent event)
+	{
+		
+		emf = getEntityManagerFactory();
+		EntityManagerProvider.INSTANCE.setEntityManagerFactory(emf);
+	}
+
+
+	@Override
+	public void contextDestroyed(ServletContextEvent event)
+	{
+		emf.close();
+	}
+
+	/**
+	 * Implement this method to provide a factory. e.g.
+	 * 
+	 *  return Persistence.createEntityManagerFactory("scoutmaster");
+
+	 * @return an EntityManagerFactory
+	 */
+	abstract protected EntityManagerFactory getEntityManagerFactory();
+
+
+}
