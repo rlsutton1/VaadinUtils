a5361fc2baf616145d12d75f5e57b51b9ece1742
bsutton@noojee.com.au <bsutton@noojee.com.au>
2013-12-07 08:08:47 +1100

    removed warnings.

--- src/main/java/au/com/vaadinutils/crud/example/entities/TblExample.java
+++ src/main/java/au/com/vaadinutils/crud/example/entities/TblExample.java
@@ -4,13 +4,10 @@ import java.io.Serializable;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.validation.constraints.NotNull;
 
 import au.com.vaadinutils.crud.CrudEntity;
 
