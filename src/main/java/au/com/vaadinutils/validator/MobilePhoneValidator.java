fb95632c9f43005d4d52db1c7d7d85d7790f0f99
bsutton@noojee.com.au <bsutton@noojee.com.au>
2014-01-31 18:29:31 +1100

    expanded the range of valid mobile no.s.

--- src/main/java/au/com/vaadinutils/validator/MobilePhoneValidator.java
+++ src/main/java/au/com/vaadinutils/validator/MobilePhoneValidator.java
@@ -4,9 +4,7 @@ import com.vaadin.data.validator.RegexpValidator;
 
 
 /**
- * String validator for e-mail addresses. The e-mail address syntax is not
- * complete according to RFC 822 but handles the vast majority of valid e-mail
- * addresses correctly.
+ * String validator for australia mobile phones.
  * 
  * See {@link com.vaadin.data.validator.AbstractStringValidator} for more
  * information.
@@ -16,23 +14,19 @@ import com.vaadin.data.validator.RegexpValidator;
  * fields or a separate validator (or override {@link #isValidValue(String)}) to
  * fail on empty values.
  * </p>
- * 
- * @author Vaadin Ltd.
- * @since 5.4
  */
 @SuppressWarnings("serial")
 public class MobilePhoneValidator extends RegexpValidator {
 
     /**
-     * Creates a validator for checking that a string is a syntactically valid
-     * e-mail address.
+     * Creates a validator for checking that a string is a valid australian mobile no.
      * 
      * @param errorMessage
      *            the message to display in case the value does not validate.
      */
     public MobilePhoneValidator(String errorMessage) {
         super(
-                "^([0-9]){10}",
+                "^(614|04)[0-9]{8}",
                 true, errorMessage.replaceAll("\\s",""));
     }
 }
