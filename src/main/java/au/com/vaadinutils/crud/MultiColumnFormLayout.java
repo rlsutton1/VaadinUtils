4ff81fd52bd1990c03275886490ee6510dda1d4f
bsutton@noojee.com.au <bsutton@noojee.com.au>
2014-02-15 10:40:39 +1100

    Fixed a bug in the addTextField metho which caused it to be added outside of the grid.

--- src/main/java/au/com/vaadinutils/crud/MultiColumnFormLayout.java
+++ src/main/java/au/com/vaadinutils/crud/MultiColumnFormLayout.java
@@ -9,12 +9,13 @@ import javax.persistence.metamodel.SingularAttribute;
 import org.apache.log4j.Logger;
 
 import au.com.vaadinutils.crud.splitFields.SplitField;
+import au.com.vaadinutils.domain.Color;
 import au.com.vaadinutils.fields.CKEditorEmailField;
-import au.com.vaadinutils.fields.Color;
 import au.com.vaadinutils.fields.ColorPickerField;
 
 import com.vaadin.shared.ui.datefield.Resolution;
 import com.vaadin.ui.AbstractComponent;
+import com.vaadin.ui.AbstractLayout;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.CheckBox;
 import com.vaadin.ui.ComboBox;
@@ -263,14 +264,11 @@ public class MultiColumnFormLayout<E> extends VerticalLayout
 	 * @param caption
 	 * @return
 	 */
-	public TextField addTextField(String caption)
+	public TextField addTextField(String fieldLabel)
 	{
-		TextField field = new TextField(caption);
-		field.setWidth("100%");
-		field.setImmediate(true);
-		field.setNullRepresentation("");
-		field.setNullSettingAllowed(false);
-		this.addComponent(field);
+		TextField field = formHelper.bindTextField((AbstractLayout)this, (ValidatingFieldGroup<E>) null, fieldLabel,
+				(String) null);
+		
 		this.fieldList.add(field);
 		return field;
 	}
@@ -299,8 +297,6 @@ public class MultiColumnFormLayout<E> extends VerticalLayout
 	 */
 	public PasswordField addPasswordField(String fieldLabel)
 	{
-		// PasswordField field = new PasswordField(caption);
-
 		PasswordField field = formHelper.bindPasswordField(this, (ValidatingFieldGroup<?>) null, fieldLabel,
 				(SingularAttribute<E, String>) null);
 		this.fieldList.add(field);
