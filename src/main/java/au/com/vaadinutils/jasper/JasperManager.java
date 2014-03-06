d6eb109e0632a974ccf60d751b20406bbd82af38
bsutton@noojee.com.au <bsutton@noojee.com.au>
2014-03-06 13:28:35 +1100

    goto report working directly with the jasper api rather than having to go through a servlet.

--- src/main/java/au/com/vaadinutils/jasper/JasperManager.java
+++ src/main/java/au/com/vaadinutils/jasper/JasperManager.java
@@ -4,13 +4,11 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 
 import javax.activation.DataSource;
-import javax.activation.FileDataSource;
 import javax.persistence.EntityManager;
 
 import net.sf.jasperreports.engine.JRAbstractExporter;
@@ -20,24 +18,24 @@ import net.sf.jasperreports.engine.JRParameter;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.export.JRCsvExporter;
 import net.sf.jasperreports.engine.export.JRHtmlExporter;
 import net.sf.jasperreports.engine.export.JRHtmlExporterParameter;
 import net.sf.jasperreports.engine.export.JRPdfExporter;
 import net.sf.jasperreports.engine.util.JRLoader;
 
 import org.apache.commons.mail.ByteArrayDataSource;
-import org.apache.commons.mail.EmailException;
-import org.apache.commons.mail.ImageHtmlEmail;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import au.com.vaadinutils.dao.Transaction;
 
 import com.google.gwt.thirdparty.guava.common.base.Preconditions;
+import com.vaadin.server.Page;
+import com.vaadin.server.VaadinServlet;
 
 public class JasperManager
 {
-	@SuppressWarnings("unused")
 	private static Logger logger = LogManager.getLogger(JasperManager.class);
 
 	public enum Disposition
@@ -46,16 +44,41 @@ public class JasperManager
 
 	}
 
-	public enum Exporter
+	public enum OutputFormat
 	{
-		PDF, HTML;
+		PDF
+		{
+			@Override
+			public String getMimeType()
+			{
+				return "application/pdf";
+			}
+		},
+		HTML
+		{
+			@Override
+			public String getMimeType()
+			{
+				return "text/html";
+			}
+		},
+		CSV
+		{
+			@Override
+			public String getMimeType()
+			{
+				return "text/csv";
+			}
+		};
+
+		abstract public String getMimeType();
 	}
 
 	final private JasperReport jasperReport;
 	final Map<String, Object> boundParams = new HashMap<String, Object>();
-	private JasperPrint jasper_print;
 	private JasperSettings settings;
 	private EntityManager em;
+	private String reportName;
 
 	/**
 	 * 
@@ -63,14 +86,15 @@ public class JasperManager
 	 *            path to jasper report.
 	 * @throws JRException
 	 */
-	public JasperManager(EntityManager em, File jasperReportFile, JasperSettings settings) throws JRException
+	public JasperManager(EntityManager em, String reportName, JasperSettings settings) throws JRException
 	{
-		Preconditions.checkArgument(jasperReportFile.exists(), "The passed Jasper Report File doesn't exist: "
-				+ jasperReportFile.getAbsolutePath());
+		Preconditions.checkArgument(settings.getReportFile(reportName).exists(),
+				"The passed Jasper Report File doesn't exist: " + settings.getReportFile(reportName).getAbsolutePath());
 
 		this.em = em;
 		this.settings = settings;
-		this.jasperReport = (JasperReport) JRLoader.loadObject(jasperReportFile);
+		this.reportName = reportName;
+		this.jasperReport = (JasperReport) JRLoader.loadObject(settings.getReportFile(reportName));
 
 	}
 
@@ -98,8 +122,10 @@ public class JasperManager
 		}
 	}
 
-	public void fillReport() throws JRException
+	private JasperPrint fillReport() throws JRException
 	{
+		JasperPrint jasper_print;
+
 		Transaction t = new Transaction(em);
 		try
 		{
@@ -112,16 +138,13 @@ public class JasperManager
 		}
 		finally
 		{
-			
-				t.close();
+
+			t.close();
 		}
+		
+		return jasper_print;
 	}
 
-	boolean isEmpty()
-	{
-		Preconditions.checkArgument(jasper_print != null, "You must call fillReport first.");
-		return (jasper_print.getPages().size() < 1);
-	}
 
 	public JasperSettings getSettings()
 	{
@@ -143,22 +166,34 @@ public class JasperManager
 		return exists;
 	}
 
-	public RenderedReport export(Exporter exportMethod) throws JRException, IOException
+	public RenderedReport export(OutputFormat exportMethod) throws JRException, IOException
 	{
 
 		final ByteArrayOutputStream out = new ByteArrayOutputStream();
 		final HashMap<String, byte[]> images = new HashMap<String, byte[]>();
 		JRAbstractExporter exporter = null;
+		
+		JasperPrint jasper_print = fillReport();
+		
+		String renderedName = this.jasperReport.getName();
+
 
 		switch (exportMethod)
 		{
 			case HTML:
 			{
 				exporter = new JRHtmlExporter();
+
 				exporter.setParameter(JRExporterParameter.JASPER_PRINT, jasper_print);
 				exporter.setParameter(JRExporterParameter.OUTPUT_STREAM, out);
 				exporter.setParameter(JRHtmlExporterParameter.IMAGES_MAP, images);
-				exporter.setParameter(JRHtmlExporterParameter.IMAGES_URI, "");
+
+				String context = VaadinServlet.getCurrent().getServletContext().getContextPath();
+				int contextIndex = Page.getCurrent().getLocation().toString().indexOf(context);
+				String baseurl = Page.getCurrent().getLocation().toString().substring(0, contextIndex + context.length() + 1);
+				exporter.setParameter(JRHtmlExporterParameter.IMAGES_URI, baseurl + "images?image=");
+				
+				renderedName += ".htm";
 				break;
 			}
 			case PDF:
@@ -166,27 +201,41 @@ public class JasperManager
 				exporter = new JRPdfExporter();
 				exporter.setParameter(JRExporterParameter.JASPER_PRINT, jasper_print);
 				exporter.setParameter(JRExporterParameter.OUTPUT_STREAM, out);
+				renderedName += ".pdf";
+				break;
+			}
+			case CSV:
+			{
+				exporter = new JRCsvExporter();
+				exporter.setParameter(JRExporterParameter.JASPER_PRINT, jasper_print);
+				exporter.setParameter(JRExporterParameter.OUTPUT_STREAM, out);
+				renderedName += ".csv";
 				break;
 			}
 			default:
 			{
-				throw new RuntimeException("Unsupported export option "+exportMethod);
-				}
-			
-		}
-
-		for (Entry<JRExporterParameter, Object> param : exporter.getParameters().entrySet())
-		{
-			System.out.println(param.getKey() + ":" + param.getValue());
+				throw new RuntimeException("Unsupported export option " + exportMethod);
+			}
 
 		}
-		exporter.exportReport();
 
-		for (Entry<JRExporterParameter, Object> param : exporter.getParameters().entrySet())
-		{
-			System.out.println(param.getKey() + ":" + param.getValue());
+		if (logger.isDebugEnabled())
+			for (Entry<JRExporterParameter, Object> param : exporter.getParameters().entrySet())
+			{
+				logger.debug("{} : {}", param.getKey(), param.getValue());
 
-		}
+			}
+
+		// InputStream stream =
+		// JasperRunManager.runReportToPdf(getClass().getClassLoader().getResourceAsStream(settings.getReportFile(this.reportName)),
+		// map, con);
+
+		if (logger.isDebugEnabled())
+			for (Entry<JRExporterParameter, Object> param : exporter.getParameters().entrySet())
+			{
+				logger.debug("{} : {}", param.getKey(), param.getValue());
+
+			}
 
 		final DataSource[] imagesrcs = (images.size() <= 0) ? null : new DataSource[images.size()];
 		if (imagesrcs != null)
@@ -200,144 +249,9 @@ public class JasperManager
 			}
 		}
 
-		return new RenderedReport(out, imagesrcs, exportMethod);
-	}
+		exporter.exportReport();
 
-	public static class EmailBuilder
-	{
-		private JasperSettings settings;
-
-		private ArrayList<DataSource> attachments = new ArrayList<DataSource>();
-		private ArrayList<String> tos = new ArrayList<String>();
-		private ArrayList<String> ccs = new ArrayList<String>();
-		private ArrayList<String> bccs = new ArrayList<String>();
-		private String subject;
-		private String fromAddress;
-
-		// Different body types
-		private RenderedReport renderedReportBody;
-		private String htmlBody;
-		private String textBody = "Your email client does not support HTML messages";
-
-		public EmailBuilder(JasperSettings settings)
-		{
-			this.settings = settings;
-		}
-
-		public EmailBuilder addTo(String toAddress)
-		{
-			this.tos.add(toAddress);
-			return this;
-		}
-
-		public EmailBuilder addCC(String ccAddress)
-		{
-			this.ccs.add(ccAddress);
-			return this;
-		}
-
-		public EmailBuilder addBCC(String bccAddress)
-		{
-			this.bccs.add(bccAddress);
-			return this;
-		}
-
-		public EmailBuilder setSubject(String subject)
-		{
-			this.subject = subject;
-			return this;
-		}
-
-		public EmailBuilder setFrom(String fromAddress)
-		{
-			this.fromAddress = fromAddress;
-			return this;
-		}
-
-		public EmailBuilder setHtmlBody(RenderedReport renderedReport)
-		{
-			Preconditions.checkArgument(this.htmlBody == null, "You may only call one of the setXXXBody methods.");
-			Preconditions.checkArgument(renderedReport.isHTML(), "The report has not been exported to HTML.");
-			this.renderedReportBody = renderedReport;
-			return this;
-		}
-
-		public EmailBuilder setHtmlBody(String htmlBody)
-		{
-			Preconditions.checkArgument(this.renderedReportBody == null,
-					"You may only call one of the setXXXBody methods.");
-			this.htmlBody = htmlBody;
-			return this;
-		}
-
-		public EmailBuilder setTextBody(String body)
-		{
-			this.textBody = body;
-			return this;
-		}
-
-		public EmailBuilder addAttachement(File attachement)
-		{
-			this.attachments.add(new FileDataSource(attachement));
-			return this;
-		}
-
-		public EmailBuilder addAttachement(DataSource attachement)
-		{
-			this.attachments.add(attachement);
-			return this;
-		}
-
-		public void send() throws EmailException
-		{
-
-			Preconditions.checkNotNull(fromAddress);
-			Preconditions.checkNotNull(tos.size() > 0);
-			Preconditions.checkNotNull(subject);
-			Preconditions.checkNotNull(this.htmlBody != null || this.renderedReportBody != null,
-					"You must specify a body.");
-
-			ImageHtmlEmail email = new ImageHtmlEmail();
-			if (this.renderedReportBody != null)
-				email.setDataSourceResolver(new JasperDataSourceResovler(renderedReportBody));
-
-			email.setDebug(true);
-			email.setHostName(settings.getSmtpFQDN());
-			email.setSmtpPort(settings.getSmtpPort());
-			if (settings.isAuthRequired())
-				email.setAuthentication(settings.getUsername(), settings.getPassword());
-			if (settings.getUseSSL())
-			{
-				email.setSslSmtpPort(settings.getSmtpPort().toString());
-				email.setSSLOnConnect(true);
-				email.setSSLCheckServerIdentity(false);
-			}
-			email.setFrom(fromAddress);
-			email.setBounceAddress(settings.getBounceEmailAddress());
-			email.setSubject(subject);
-
-			for (String to : this.tos)
-				email.addTo(to);
-
-			for (String cc : this.ccs)
-				email.addCc(cc);
-
-			for (String bcc : this.bccs)
-				email.addBcc(bcc);
-
-			if (this.htmlBody != null)
-				email.setHtmlMsg(this.htmlBody);
-
-			if (this.renderedReportBody != null)
-				email.setHtmlMsg(this.renderedReportBody.getBodyAsHtml());
-
-			email.setTextMsg(this.textBody);
-
-			for (DataSource attachment : this.attachments)
-				email.attach(attachment, attachment.getName(), attachment.getName());
-
-			email.send();
-		}
+		return new RenderedReport(this, renderedName, out, imagesrcs, exportMethod);
 	}
 
 	protected static String getJasperFile(final String dir, final String name) throws IOException
@@ -346,6 +260,11 @@ public class JasperManager
 		if (!file.exists())
 			throw new FileNotFoundException(file.toString());
 		return file.getCanonicalPath();
+	}
+
+	public String getReportName()
+	{
+		return reportName;
 	}
 
 }
--- src/main/java/au/com/vaadinutils/jasper/RenderedReport.java
+++ src/main/java/au/com/vaadinutils/jasper/RenderedReport.java
@@ -1,23 +1,37 @@
 package au.com.vaadinutils.jasper;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 
 import javax.activation.DataSource;
 
 import org.apache.commons.mail.ByteArrayDataSource;
 
-import au.com.vaadinutils.jasper.JasperManager.Exporter;
+import au.com.vaadinutils.jasper.JasperManager.OutputFormat;
 
+import com.vaadin.server.Resource;
+import com.vaadin.server.StreamResource;
+import com.vaadin.server.StreamResource.StreamSource;
+
+/*
+ * Holds an in memory copy of a Rendered Jasper Report.
+ */
 public class RenderedReport
 {
 	private final ByteArrayOutputStream reportBody;
 	private final DataSource[] images;
-	private Exporter exportMethod;
+	private OutputFormat exportMethod;
+	private String name;
+	@SuppressWarnings("unused")
+	private JasperManager manager;
 
-	RenderedReport(ByteArrayOutputStream out, DataSource[] images, Exporter exportMethod)
+	RenderedReport(JasperManager manager, String reportName, ByteArrayOutputStream out, DataSource[] images, OutputFormat exportMethod)
 	{
+		this.manager = manager;
+		this.name = reportName;
 		this.reportBody = out;
 		this.images = images;
 		this.exportMethod = exportMethod;
@@ -25,12 +39,17 @@ public class RenderedReport
 
 	public boolean isHTML()
 	{
-		return exportMethod == Exporter.HTML;
+		return exportMethod == OutputFormat.HTML;
 	}
 	
 	public boolean isPDF()
 	{
-		return exportMethod == Exporter.PDF;
+		return exportMethod == OutputFormat.PDF;
+	}
+
+	public boolean isCsv()
+	{
+		return exportMethod == OutputFormat.CSV;
 	}
 
 	public String getBodyAsHtml()
@@ -55,5 +74,96 @@ public class RenderedReport
 	{
 		return images;
 	}
+
+	public Resource getBodyAsResource()
+	{
+		//new ByteArrayDataSource(entry.getValue(), "image/gif");
+		
+		// TODO: can we stream this.
+		JasperStreamSource jss = new JasperStreamSource(new ByteArrayInputStream(reportBody.toByteArray()));
+		
+
+//        StreamResource resource = new JasperStreamSource(source, "TokenReport.pdf", getApplication());
+//        resource.setMIMEType("application/pdf");
+//
+		
 	
+//
+		StreamResource streamResource = new StreamResource(jss, this.name);
+		streamResource.setMIMEType(exportMethod.getMimeType());
+		
+		return streamResource;
+	}
+	
+	static class JasperStreamSource implements StreamSource
+	{
+		private static final long serialVersionUID = 1L;
+		private InputStream stream;
+
+		JasperStreamSource(InputStream stream)
+		{
+			this.stream = stream;
+//	                byte[] b = null;
+//	                try {
+//	                	RenderReported.this.manager.
+//	                } catch (JRException ex) {
+//	                    Logger.getLogger(TokenForm.class.getName()).log(Level.SEVERE, null, ex);
+//	                }
+//	                stream = new ByteArrayInputStream(b);
+//	        };
+			
+			
+		}
+
+		@Override
+		public InputStream getStream()
+		{
+			
+			return this.stream;
+		}
+		
+		
+	}
+	
+//	 try {
+//         //FileOutputStream of = new FileOutputStream("TokenReport.pdf");
+//         //JasperRunManager.runReportToPdfStream(getClass().getClassLoader().getResourceAsStream("reports/TokenReport.jasper"), of, map, con);
+//
+//         StreamResource.StreamSource source = new StreamResource.StreamSource() {
+//
+//             public InputStream getStream() {
+//                 byte[] b = null;
+//                 try {
+//                     b = JasperRunManager.runReportToPdf(getClass().getClassLoader().getResourceAsStream("reports/TokenReport.jasper"), map, con);
+//                 } catch (JRException ex) {
+//                     Logger.getLogger(TokenForm.class.getName()).log(Level.SEVERE, null, ex);
+//                 }
+//                 //throw new UnsupportedOperationException("Not supported yet.");
+//                 return new ByteArrayInputStream(b);
+//             }
+//         };
+//
+//         StreamResource resource = new StreamResource(source, "TokenReport.pdf", getApplication());
+//         resource.setMIMEType("application/pdf");
+
+//         Window w = new Window("Token Form");
+//         w.setSizeFull();
+//         //getWindow().addWindow(w);
+//
+//         Embedded e = new Embedded();
+//         e.setMimeType("application/pdf");
+//         e.setType(Embedded.TYPE_OBJECT);
+//         e.setSizeFull();
+//         e.setSource(resource);
+//         e.setParameter("Content-Disposition", "attachment; filename=" + resource.getFilename());
+//
+//         w.addComponent(e);
+
+//         getApplication().getMainWindow().open(resource, "_new");
+//         
+//
+//     } catch (Exception ex) {
+//         Logger.getLogger(TokenForm.class.getName()).log(Level.SEVERE, null, ex);
+//     }
+//	
 }
