55add5ffb8d4b31114e6ca9f1646aeb38fa3e52f
Bator Horvath <bhorvath@noojee.com.au>
2014-03-28 14:54:34 +1100

    Add optional Refresher to WorkingDialog

--- src/main/java/au/com/vaadinutils/ui/WorkingDialog.java
+++ src/main/java/au/com/vaadinutils/ui/WorkingDialog.java
@@ -5,6 +5,7 @@ import au.com.vaadinutils.listener.ClickEventLogged;
 import au.com.vaadinutils.listener.CompleteListener;
 import au.com.vaadinutils.listener.ProgressListener;
 
+import com.github.wolfie.refresher.Refresher;
 import com.vaadin.shared.ui.label.ContentMode;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
@@ -16,22 +17,22 @@ import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.Window;
 
 /**
- * Displays a dialog designed to be shown when a long running task is in progress.
+ * Displays a dialog designed to be shown when a long running task is in
+ * progress.
  * 
  * You can use WorkingDialog in one of two ways.
  * 
- * 1) Call WorkingDialog
- * Add it to the UI via UI.getCurrent().addWindow(workingDialog);
- * Then set a runnable by calling setWorker(new Runnable() {} );
- * The working dialog will then display its self and run the Runnable in a background thread.
- * When the Runnable completes the WorkingDialog will be removed from the UI.
+ * 1) Call WorkingDialog Add it to the UI via
+ * UI.getCurrent().addWindow(workingDialog); Then set a runnable by calling
+ * setWorker(new Runnable() {} ); The working dialog will then display its self
+ * and run the Runnable in a background thread. When the Runnable completes the
+ * WorkingDialog will be removed from the UI.
  * 
- * 2) Call WorkingDialog
- * Add it to the UI via UI.getCurrent().addWindow(workingDialog);
- * Pass it to your own thread as a 'ProgressListener'. When your thread calls either the complete or exception methods
- * the Working Dialog will be closed.
- * Calls to itemError are ignored.
- *   
+ * 2) Call WorkingDialog Add it to the UI via
+ * UI.getCurrent().addWindow(workingDialog); Pass it to your own thread as a
+ * 'ProgressListener'. When your thread calls either the complete or exception
+ * methods the Working Dialog will be closed. Calls to itemError are ignored.
+ * 
  */
 
 public class WorkingDialog extends Window implements ProgressListener<String>
@@ -43,29 +44,55 @@ public class WorkingDialog extends Window implements ProgressListener<String>
 	private CancelListener cancelListener;
 	private CompleteListener completeListener;
 
-	
 	/**
-	 * Displays a dialog designed to be shown when  
+	 * Displays a dialog designed to be shown when a long running task is in
+	 * progress.
+	 * 
 	 * @param caption
 	 * @param message
 	 */
 	public WorkingDialog(String caption, String message)
 	{
-		this(caption, message, null);
+		this(caption, message, null, null);
 	}
-	
 
 	/**
-	 * Display the Working Dialog with a Cancel Button.
-	 * If the user clicks the Cancel button the listener will be sent
-	 * a cancel button.
-	 * The setWorker method does not support being cancelled.
+	 * Displays a dialog designed to be shown when a long running task is in
+	 * progress.
 	 * 
 	 * @param caption
 	 * @param message
 	 * @param listener
 	 */
 	public WorkingDialog(String caption, String message, CancelListener listener)
+	{
+		this(caption, message, listener, null);
+	}
+
+	/**
+	 * Displays a dialog designed to be shown when a long running task is in
+	 * progress.
+	 * 
+	 * @param caption
+	 * @param message
+	 * @param refresher
+	 */
+	public WorkingDialog(String caption, String message, Refresher refresher)
+	{
+		this(caption, message, null, refresher);
+	}
+
+	/**
+	 * Display the Working Dialog with a Cancel Button. If the user clicks the
+	 * Cancel button the listener will be sent a cancel button. The setWorker
+	 * method does not support being cancelled.
+	 * 
+	 * @param caption
+	 * @param message
+	 * @param listener
+	 * @param refresher
+	 */
+	public WorkingDialog(String caption, String message, CancelListener listener, Refresher refresher)
 	{
 		super(caption);
 		this.setModal(true);
@@ -110,19 +137,24 @@ public class WorkingDialog extends Window implements ProgressListener<String>
 
 		}
 
+		if (refresher != null)
+			addExtension(refresher);
+
 		this.setContent(content);
 		this.center();
 
 	}
 
-
 	/**
-	 * Pass a Runnable that WorkingDialog will run in a background thread.
-	 * On completion of the thread the complete listener will be notified
-	 * and the WorkingDialog will remove itself rom the UI.
+	 * Pass a Runnable that WorkingDialog will run in a background thread. On
+	 * completion of the thread the complete listener will be notified and the
+	 * WorkingDialog will remove itself rom the UI.
 	 * 
-	 * @param runnable the runnable to be run in a background thread.
-	 * @param listener a complete listener to be notified when the thread has finished.
+	 * @param runnable
+	 *            the runnable to be run in a background thread.
+	 * @param listener
+	 *            a complete listener to be notified when the thread has
+	 *            finished.
 	 */
 
 	public void setWorker(Runnable runnable, CompleteListener listener)
@@ -167,7 +199,6 @@ public class WorkingDialog extends Window implements ProgressListener<String>
 
 	}
 
-
 	public void progress(int count, int max, final String message)
 	{
 		new UIUpdater(new Runnable()
@@ -187,17 +218,13 @@ public class WorkingDialog extends Window implements ProgressListener<String>
 		WorkingDialog.this.close();
 
 	}
-	
-
-
 
 	@Override
 	public void itemError(Exception e, String status)
 	{
 		// Ignored.
-		
-	}
 
+	}
 
 	@Override
 	public void exception(Exception e)
