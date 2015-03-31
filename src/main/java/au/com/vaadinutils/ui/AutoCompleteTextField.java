669cc2eb4c20aab71d061ecbb34e3c2180fd2588
Bator Horvath <bhorvath@noojee.com.au>
2015-04-01 10:53:21 +1100

    Allow listeners to be removed

--- src/main/java/au/com/vaadinutils/ui/AutoCompleteTextField.java
+++ src/main/java/au/com/vaadinutils/ui/AutoCompleteTextField.java
@@ -16,104 +16,117 @@ import com.vaadin.ui.TextField;
 public class AutoCompleteTextField<E> extends TextField
 {
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    private AutoCompeleteQueryListener<E> listener;
+	private AutoCompeleteQueryListener<E> listener;
 
-    Map<String, E> options = new LinkedHashMap<>();
+	Map<String, E> options = new LinkedHashMap<>();
 
-    private AutoCompleteOptionSelected<E> optionListener;
+	private AutoCompleteOptionSelected<E> optionListener;
 
-    private ContextMenu contextMenu;
+	private ContextMenu contextMenu;
 
-    /**
-     * <pre>
-     * {@code
-     * sample usage
-     * 
-     * 	AutoCompleteTextFieldV2<PostCode> suburb = new AutoCompleteTextFieldV2<>();
-     * 
-     * suburb.setQueryListener(new AutoCompeleteQueryListener<PostCode>()
-     * {
-     * 
-     * 	    @Override
-     * 	    public void handleQuery(AutoCompleteTextFieldV2<PostCode> field,String queryText)
-     * 	    {
-     * 		    field.addOption(new PostCode(3241),"Title");
-     * 	    }
-     * 	});
-     * 
-     * 	suburb.setOptionSelectionListener(new AutoCompleteOptionSelected<PostCode>()
-     * 	{
-     * 	    
-     * 	    @Override
-     * 	    public void optionSelected(AutoCompleteTextFieldV2<PostCode> field, PostCode option)
-     * 	    {
-     * 		field.setValue(option.getSuburb());
-     * 	    }
-     * 	});
-     * }
-     * </pre>
-     */
+	/**
+	 * <pre>
+	 * {@code
+	 * sample usage
+	 * 
+	 * 	AutoCompleteTextFieldV2<PostCode> suburb = new AutoCompleteTextFieldV2<>();
+	 * 
+	 * suburb.setQueryListener(new AutoCompeleteQueryListener<PostCode>()
+	 * {
+	 * 
+	 * 	    @Override
+	 * 	    public void handleQuery(AutoCompleteTextFieldV2<PostCode> field,String queryText)
+	 * 	    {
+	 * 		    field.addOption(new PostCode(3241),"Title");
+	 * 	    }
+	 * 	});
+	 * 
+	 * 	suburb.setOptionSelectionListener(new AutoCompleteOptionSelected<PostCode>()
+	 * 	{
+	 * 	    
+	 * 	    @Override
+	 * 	    public void optionSelected(AutoCompleteTextFieldV2<PostCode> field, PostCode option)
+	 * 	    {
+	 * 		field.setValue(option.getSuburb());
+	 * 	    }
+	 * 	});
+	 * }
+	 * </pre>
+	 */
 
-    public AutoCompleteTextField()
-    {
-
-	contextMenu = new ContextMenu();
-	contextMenu.setAsContextMenuOf(this);
-	contextMenu.setOpenAutomatically(false);
-
-	setTextChangeEventMode(TextChangeEventMode.LAZY);
-	setImmediate(true);
-	addTextChangeListener(new TextChangeListener()
+	public AutoCompleteTextField()
 	{
-	    private static final long serialVersionUID = 1L;
 
-	    public void textChange(final TextChangeEvent event)
-	    {
-		options.clear();
-		listener.handleQuery(AutoCompleteTextField.this, event.getText());
-		showOptionMenu();
-	    }
+		contextMenu = new ContextMenu();
+		contextMenu.setAsContextMenuOf(this);
+		contextMenu.setOpenAutomatically(false);
 
-	});
-
-    }
-
-    private void showOptionMenu()
-    {
-
-	contextMenu.removeAllItems();
-	contextMenu.open(this);
-
-	for (final Entry<String, E> option : options.entrySet())
-	{
-	    ContextMenuItem menuItem = contextMenu.addItem(option.getKey());
-	    menuItem.addItemClickListener(new ContextMenuItemClickListener()
-	    {
-
-		@Override
-		public void contextMenuItemClicked(ContextMenuItemClickEvent event)
+		setTextChangeEventMode(TextChangeEventMode.LAZY);
+		setImmediate(true);
+		addTextChangeListener(new TextChangeListener()
 		{
-		    optionListener.optionSelected(AutoCompleteTextField.this, option.getValue());
-		}
-	    });
+			private static final long serialVersionUID = 1L;
+
+			public void textChange(final TextChangeEvent event)
+			{
+				options.clear();
+				if (listener != null)
+				{
+					listener.handleQuery(AutoCompleteTextField.this, event.getText());
+				}
+				showOptionMenu();
+			}
+
+		});
+
 	}
 
-    }
+	private void showOptionMenu()
+	{
 
-    public void setOptionSelectionListener(AutoCompleteOptionSelected<E> listener)
-    {
-	this.optionListener = listener;
-    }
+		contextMenu.removeAllItems();
+		contextMenu.open(this);
 
-    public void setQueryListener(AutoCompeleteQueryListener<E> listener)
-    {
-	this.listener = listener;
-    }
+		for (final Entry<String, E> option : options.entrySet())
+		{
+			ContextMenuItem menuItem = contextMenu.addItem(option.getKey());
+			menuItem.addItemClickListener(new ContextMenuItemClickListener()
+			{
 
-    public void addOption(E option, String optionLabel)
-    {
-	options.put(optionLabel, option);
-    }
+				@Override
+				public void contextMenuItemClicked(ContextMenuItemClickEvent event)
+				{
+					optionListener.optionSelected(AutoCompleteTextField.this, option.getValue());
+				}
+			});
+		}
+
+	}
+
+	public void setOptionSelectionListener(AutoCompleteOptionSelected<E> listener)
+	{
+		this.optionListener = listener;
+	}
+
+	public void removeOptionSelectionListener()
+	{
+		optionListener = null;
+	}
+
+	public void setQueryListener(AutoCompeleteQueryListener<E> listener)
+	{
+		this.listener = listener;
+	}
+
+	public void removeQueryListener()
+	{
+		listener = null;
+	}
+
+	public void addOption(E option, String optionLabel)
+	{
+		options.put(optionLabel, option);
+	}
 }
