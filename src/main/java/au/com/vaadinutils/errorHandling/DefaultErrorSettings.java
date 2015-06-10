52f6ac990e3567277ddb3917b63bbdb5cc886ff2
rsutton <rlsutton1@optusnet.com.au>
2015-06-10 10:11:28 +1000

    add better messages to help with usage for errorwindow

--- src/main/java/au/com/vaadinutils/errorHandling/DefaultErrorSettings.java
+++ src/main/java/au/com/vaadinutils/errorHandling/DefaultErrorSettings.java
@@ -8,25 +8,25 @@ public class DefaultErrorSettings implements ErrorSettings
 	@Override
 	public String getSystemName()
 	{
-		return "System name not set";
+		return "System name not set, call ErrorSettingsFactory.setErrorSettings and implement appropriate settings";
 	}
 
 	@Override
 	public String getViewName()
 	{
-		return "View not know";
+		return "View not know, call ErrorSettingsFactory.setErrorSettings and implement appropriate settings";
 	}
 
 	@Override
 	public String getUserName()
 	{
-		return "User not known";
+		return "User not known, call ErrorSettingsFactory.setErrorSettings and implement appropriate settings";
 	}
 
 	@Override
 	public String getTargetEmailAddress()
 	{
-		return "Target email address not set";
+		return "Target email address not set, call ErrorSettingsFactory.setErrorSettings and implement appropriate settings";
 	}
 
 	@Override
