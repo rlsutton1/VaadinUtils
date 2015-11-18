146696630444c4414391a8afd1d939c9ca3749bc
Robert Sutton <rlsutton1@optusnet.com.au>
2015-11-18 13:52:00 +1100

    re-format

--- src/main/java/au/com/vaadinutils/crud/SelectableEntityTable.java
+++ src/main/java/au/com/vaadinutils/crud/SelectableEntityTable.java
@@ -8,60 +8,60 @@ import com.vaadin.data.Container;
 
 public class SelectableEntityTable<E> extends TableCheckBoxSelect
 {
-    // private static transient Logger logger =
-    // LogManager.getLogger(SelectableEntityTable.class);
+	// private static transient Logger logger =
+	// LogManager.getLogger(SelectableEntityTable.class);
 
-    private static final long serialVersionUID = 1L;
-    private String uniqueId;
+	private static final long serialVersionUID = 1L;
+	private String uniqueId;
 
-    public SelectableEntityTable(Container.Filterable childContainer, HeadingPropertySet<E> headingPropertySet,
-	    String uniqueId)
-    {
-	super();
-	this.uniqueId = uniqueId;
-	setContainerDataSource(childContainer);
-	buildSelectableContainer(headingPropertySet);
+	public SelectableEntityTable(Container.Filterable childContainer, HeadingPropertySet<E> headingPropertySet,
+			String uniqueId)
+	{
+		super();
+		this.uniqueId = uniqueId;
+		setContainerDataSource(childContainer);
+		buildSelectableContainer(headingPropertySet);
 
-    }
+	}
 
-    /**
-     * copy the containers items into a new IndexContainer which also contains
-     * the 'selectable' property.
-     * 
-     * @param entityContainer2
-     * @param headingPropertySet2
-     * @return
-     */
-    private void buildSelectableContainer(HeadingPropertySet<E> visibleColumns)
-    {
-	visibleColumns.applyToTable(this, uniqueId);
+	/**
+	 * copy the containers items into a new IndexContainer which also contains
+	 * the 'selectable' property.
+	 * 
+	 * @param entityContainer2
+	 * @param headingPropertySet2
+	 * @return
+	 */
+	private void buildSelectableContainer(HeadingPropertySet<E> visibleColumns)
+	{
+		visibleColumns.applyToTable(this, uniqueId);
 
-	setColumnHeader(TableCheckBoxSelect.TABLE_CHECK_BOX_SELECT, "");
+		setColumnHeader(TableCheckBoxSelect.TABLE_CHECK_BOX_SELECT, "");
 
-    }
+	}
 
-    /**
-     * returns an array of selected entities.
-     * 
-     * @return
-     */
-    @SuppressWarnings("unchecked")
-    public Collection<Long> getSelectedIds()
-    {
-	return (Collection<Long>) super.getSelectedItems();
-    }
+	/**
+	 * returns an array of selected entities.
+	 * 
+	 * @return
+	 */
+	@SuppressWarnings("unchecked")
+	public Collection<Long> getSelectedIds()
+	{
+		return (Collection<Long>) super.getSelectedItems();
+	}
 
-    public void applyFilter(final Filter filter)
-    {
-	/* Reset the filter for the Entity Container. */
-	resetFilters();
-	((Container.Filterable) getContainerDataSource()).addContainerFilter(filter);
+	public void applyFilter(final Filter filter)
+	{
+		/* Reset the filter for the Entity Container. */
+		resetFilters();
+		((Container.Filterable) getContainerDataSource()).addContainerFilter(filter);
 
-    }
+	}
 
-    protected void resetFilters()
-    {
-	((Container.Filterable) getContainerDataSource()).removeAllContainerFilters();
-    }
+	protected void resetFilters()
+	{
+		((Container.Filterable) getContainerDataSource()).removeAllContainerFilters();
+	}
 
 }
