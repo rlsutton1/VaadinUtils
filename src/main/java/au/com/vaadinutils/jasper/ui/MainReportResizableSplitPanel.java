5da6aee13f614cceb4be3a374f73aca7f1a20d22
Robert Sutton <rlsutton1@optusnet.com.au>
2015-12-03 13:14:31 +1100

    allow resizing report filters panel

---
+++ src/main/java/au/com/vaadinutils/jasper/ui/MainReportResizableSplitPanel.java
@@ -0,0 +1,50 @@
+package au.com.vaadinutils.jasper.ui;
+
+import com.vaadin.ui.AbstractComponent;
+import com.vaadin.ui.Component;
+import com.vaadin.ui.HorizontalSplitPanel;
+
+public class MainReportResizableSplitPanel extends HorizontalSplitPanel implements SplitPanel
+{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -470983852015042137L;
+
+	MainReportResizableSplitPanel(int width)
+	{
+		this.setSizeFull();
+		setSplitPosition(235);
+		setMinSplitPosition(235, Unit.PIXELS);
+
+	}
+
+	@Override
+	public Component getComponent()
+	{
+		return this;
+	}
+
+	@Override
+	public void setSplitPosition(int i)
+	{
+		super.setSplitPosition(i, Unit.PIXELS);
+
+	}
+
+	@Override
+	public void setFirstComponent(AbstractComponent optionsPanel)
+	{
+		super.setFirstComponent(optionsPanel);
+
+	}
+
+	@Override
+	public void setSecondComponent(AbstractComponent splash)
+	{
+		super.setSecondComponent(splash);
+
+	}
+
+}
