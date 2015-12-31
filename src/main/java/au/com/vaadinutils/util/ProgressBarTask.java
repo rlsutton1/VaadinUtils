95ab7102c50a2303a9b41d3b2518a5263ee55776
Brett Sutton <bsutton@noojee.com.au>
2016-01-01 10:08:41 +1100

    Changed the overload to make it explicit that you need to use the passed UI as calls to UI.getCurrent() will fail on a background thread such as the on the the ProgressBarTask is normally called within.

--- src/main/java/au/com/vaadinutils/util/ProgressBarTask.java
+++ src/main/java/au/com/vaadinutils/util/ProgressBarTask.java
@@ -7,19 +7,32 @@ public abstract class ProgressBarTask<T>
 	private ProgressTaskListener<T> listener;
 
 	final UI ui;
+
 	public ProgressBarTask(ProgressTaskListener<T> listener)
 	{
 		this.listener = listener;
 		ui = UI.getCurrent();
 	}
 
-	abstract public void run();
+	public void run()
+	{
+		runUI(ui);
+	}
+
+	/**
+	 * Changed the overload to make it explicit that you need to use the passed
+	 * UI as calls to UI.getCurrent() will fail on a background thread such as
+	 * the on the the ProgressBarTask is normally called within.
+	 * 
+	 * @param ui
+	 */
+	abstract public void runUI(UI ui);
 
 	protected void taskComplete(final int sent)
 	{
+
 		ui.accessSynchronously(new Runnable()
 		{
-
 			@Override
 			public void run()
 			{
