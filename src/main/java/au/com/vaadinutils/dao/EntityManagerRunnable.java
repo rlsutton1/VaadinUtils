346b1e63c0f6be34a17d172e4c15c10f3abbc4d8
Brett Sutton <bsutton@noojee.com.au>
2016-01-01 09:09:15 +1100

    formatting changes and a comment

--- src/main/java/au/com/vaadinutils/dao/EntityManagerRunnable.java
+++ src/main/java/au/com/vaadinutils/dao/EntityManagerRunnable.java
@@ -5,17 +5,26 @@ import org.apache.logging.log4j.Logger;
 
 import com.vaadin.ui.UI;
 
+/**
+ * Overload this class to create a Runnable that has its own EntityManager and
+ * which has access to the vaadin UI object.
+ *
+ * This class should be used if you need to access a Dao object in a background
+ * thread that doesn't have an entity manager injected. This class injects the
+ * em into the thread.
+ *
+ * @author bsutton
+ *
+ */
 public abstract class EntityManagerRunnable implements Runnable
 {
 
 	final private UI ui;
 	Logger logger = LogManager.getLogger();
 
-
-
 	public EntityManagerRunnable(UI ui)
 	{
-		this.ui  =ui;
+		this.ui = ui;
 	}
 
 	@Override
@@ -37,8 +46,8 @@ public abstract class EntityManagerRunnable implements Runnable
 		}
 		catch (Exception e)
 		{
-			
-			logger.error(e,e);
+
+			logger.error(e, e);
 		}
 
 	}
