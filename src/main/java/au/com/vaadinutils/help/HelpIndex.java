06abee0127d729dbb61fa039e35356912377ac15
Robert Sutton <rsutton@noojee.com.au>
2016-10-14 11:25:09 +1100

    new slide out help panel

--- src/main/java/au/com/vaadinutils/help/HelpIndex.java
+++ src/main/java/au/com/vaadinutils/help/HelpIndex.java
@@ -5,14 +5,6 @@ import com.vaadin.ui.AbstractOrderedLayout;
 public interface HelpIndex
 {
 
-//	String lookupHelpIndex(Enum<?> helpId) throws ExecutionException;
-//
-//	String getPageUrl();
-//
-//	String getIndexPageUrl();
-
-	
-
 	void setHelpSource(Enum<?> helpId, AbstractOrderedLayout layout, HelpDisplayedCallback callback);
 
 }
