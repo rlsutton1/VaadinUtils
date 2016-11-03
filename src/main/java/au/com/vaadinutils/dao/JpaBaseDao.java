15980b2365eef39b281e60286bd8b16963c63089
Robert Sutton <rsutton@noojee.com.au>
2016-11-03 15:48:47 +1100

    added ability to select a SingularAttribute rather than an entire Entity

--- src/main/java/au/com/vaadinutils/dao/JpaBaseDao.java
+++ src/main/java/au/com/vaadinutils/dao/JpaBaseDao.java
@@ -115,6 +115,11 @@ public class JpaBaseDao<E, K> implements Dao<E, K>
 		return findById((K) new Long(id));
 	}
 
+	public <T> JpaDslSelectAttributeBuilder<E, T> find(SingularAttribute<E, T> attribute)
+	{
+		return new JpaDslSelectAttributeBuilder<>(entityClass, attribute);
+	}
+
 	@Override
 	public E findById(K id)
 	{
