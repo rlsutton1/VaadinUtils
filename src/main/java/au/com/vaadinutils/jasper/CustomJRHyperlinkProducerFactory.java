024f462d3ce6eab9eaca3262c9ee3ce941784c2c
Robert Sutton <rsutton@noojee.com.au>
2017-03-23 11:57:54 +1100

    NA-4626 fix for jasper report drill downs, switched to jasper 6 api

--- src/main/java/au/com/vaadinutils/jasper/CustomJRHyperlinkProducerFactory.java
+++ src/main/java/au/com/vaadinutils/jasper/CustomJRHyperlinkProducerFactory.java
@@ -6,30 +6,11 @@ import net.sf.jasperreports.engine.export.JRHyperlinkProducerFactory;
 public class CustomJRHyperlinkProducerFactory extends JRHyperlinkProducerFactory
 {
 
-	static final ThreadLocal<Boolean> useCustomHyperLinks = new ThreadLocal<Boolean>();
-
-	public static void setUseCustomHyperLinks(boolean b)
-	{
-		useCustomHyperLinks.set(b);
-	}
-
 	@Override
 	public JRHyperlinkProducer getHandler(String linkType)
 	{
-		if (useCustomHyperLinks.get() == null)
-		{
-			useCustomHyperLinks.set(false);
-		}
-		if (useCustomHyperLinks.get() == null)
-		{
-			useCustomHyperLinks.set(false);
-		}
-		if (useCustomHyperLinks.get())
-		{
-			return new CustomJRHyperlinkProducer();
-		}
 
-		return null;
+		return new CustomJRHyperlinkProducer();
 	}
 
 }
