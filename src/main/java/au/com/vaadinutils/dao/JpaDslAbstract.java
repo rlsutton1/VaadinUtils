fb314b9a0f8c9e5d44337a24cb11082db8d58bc3
Robert Sutton <rsutton@noojee.com.au>
2017-06-13 17:18:13 +1000

    added some methods

--- src/main/java/au/com/vaadinutils/dao/JpaDslAbstract.java
+++ src/main/java/au/com/vaadinutils/dao/JpaDslAbstract.java
@@ -204,7 +204,7 @@ public abstract class JpaDslAbstract<E, R>
 			}
 		};
 	}
-	
+
 	public <V extends Comparable<? super V>> Condition<E> between(final JoinBuilder<E, ? super V> joinBuilder,
 			final SingularAttribute<? super V, Long> field, final Long start, final Long end)
 	{
@@ -330,6 +330,11 @@ public abstract class JpaDslAbstract<E, R>
 		return builder.quot(path1, number);
 	}
 
+	public Expression<Integer> mod(final Expression<Integer> path1, int i)
+	{
+		return builder.mod(path1, i);
+	}
+
 	public Expression<?> toInteger(Expression<Number> expression)
 	{
 		return builder.toInteger(expression);
@@ -347,6 +352,19 @@ public abstract class JpaDslAbstract<E, R>
 	}
 
 	public Condition<E> eq(final Expression<String> expression, final String value)
+	{
+		return new AbstractCondition<E>()
+		{
+
+			@Override
+			public Predicate getPredicates()
+			{
+				return builder.equal(expression, value);
+			}
+		};
+	}
+
+	public<K> Condition<E> eq(final Expression<K> expression, final K value)
 	{
 		return new AbstractCondition<E>()
 		{
@@ -384,7 +402,7 @@ public abstract class JpaDslAbstract<E, R>
 			}
 		};
 	}
-	
+
 	public <J, V> Condition<E> eq(final JoinBuilder<E, J> join, final SetAttribute<J, V> field, final V value)
 	{
 		return new AbstractCondition<E>()
@@ -838,7 +856,7 @@ public abstract class JpaDslAbstract<E, R>
 			}
 		};
 	}
-	
+
 	public <V, K> Condition<E> in(final JoinBuilder<E, V> join, final ListAttribute<V, K> attribute,
 			final Collection<K> values)
 	{
