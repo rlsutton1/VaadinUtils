e2d9cf6f67b06e90e672c61273867dde3ad5cf81
Bator Horvath <bhorvath@noojee.com.au>
2017-09-01 15:42:46 +1000

    Add LazyComboBoxWithButton

---
+++ src/main/java/au/com/vaadinutils/fields/LazyComboBoxWithButton.java
@@ -0,0 +1,66 @@
+package au.com.vaadinutils.fields;
+
+import org.vaadin.viritin.LazyList;
+import org.vaadin.viritin.fields.LazyComboBox;
+import org.vaadin.viritin.fields.LazyComboBox.FilterableCountProvider;
+import org.vaadin.viritin.fields.LazyComboBox.FilterablePagingProvider;
+
+import com.vaadin.ui.Button;
+import com.vaadin.ui.CustomComponent;
+import com.vaadin.ui.HorizontalLayout;
+
+@SuppressWarnings("serial")
+public class LazyComboBoxWithButton<E> extends CustomComponent
+{
+	private Class<E> itemClass;
+	private FilterablePagingProvider<E> pagingProvider;
+	private FilterableCountProvider countProvider;
+	private int pageSize = LazyList.DEFAULT_PAGE_SIZE;
+	private Button button;
+	private LazyComboBox<E> field;
+
+	public LazyComboBoxWithButton(final Class<E> itemClass, final FilterablePagingProvider<E> pagingProvider,
+			final FilterableCountProvider countProvider, final int pageSize, final String caption, final Button button)
+	{
+		this.itemClass = itemClass;
+		this.pagingProvider = pagingProvider;
+		this.countProvider = countProvider;
+		this.pageSize = pageSize;
+
+		if (button != null)
+		{
+			this.button = button;
+		}
+		else
+		{
+			this.button = new Button();
+		}
+
+		final HorizontalLayout layout = new HorizontalLayout();
+		layout.setSizeFull();
+		layout.setSpacing(true);
+		field = createField();
+		field.setSizeFull();
+		layout.addComponent(field);
+		layout.setExpandRatio(field, 1);
+		layout.addComponent(this.button);
+
+		setCompositionRoot(layout);
+		setCaption(caption);
+	}
+
+	private LazyComboBox<E> createField()
+	{
+		return new LazyComboBox<>(itemClass, pagingProvider, countProvider, pageSize);
+	}
+
+	public LazyComboBox<E> getField()
+	{
+		return field;
+	}
+
+	public Button getButton()
+	{
+		return button;
+	}
+}
