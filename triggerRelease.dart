d9bc4794d7df2a578b6e8de89bdeaedc82f23698
Robert Sutton <rsutton@noojee.com.au>
2020-02-11 15:57:30 +1100

    for version 0.2.12

--- triggerRelease.dart
+++ triggerRelease.dart
@@ -31,6 +31,7 @@ void main() {
   'git pull'.run;
   'git add .'.run;
   'git commit -m "for version $version"'.run;
+  'git push'.run;
   'git tag -a $version -m "$version"'.run;
   'git push origin tag $version'.run;
 }
